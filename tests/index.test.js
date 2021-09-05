37bc9cb270ccb7aa3d8a2f271abe7d00c7862ba5
Antoine Torossian <torossian97@gmail.com>
2021-09-05 16:06:26 -0700

    added basic GET status test

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,10 +1,27 @@
 const chai = require('chai');
+const chaiHttp = require('chai-http');
 const config = require('config');
 const service = require('../src/service');
+
+chai.use(chaiHttp)
+
 console.log(service.port)
 
+// Test that the service is running on the proper port.
 describe('Service', ()=>{
     it('Service should be running on proper port.', async()=>{
         chai.expect(service.port).to.equal(config.get('port'))
     })
+});
+
+// Test the GET route.
+describe('/GET', () => {
+    it('Service should successfully GET 200 status', (done) => {
+      chai.request(service)
+          .get('/')
+          .end((err, res) => {
+              chai.expect(res.status).to.equal(200);
+            done();
+          });
+    });
 });
